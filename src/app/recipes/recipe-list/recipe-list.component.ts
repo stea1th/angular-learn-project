347f90c74abef5488c08e6475df2a702b0f3a6db
vlasov <stea1th@mail.ru>
2019-09-09 16:45:48 +0200

    Aufgabe 1

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Bolognese',
+      'Very tasty food',
+      'https://images-gmi-pmc.edge-generalmills.com/a8588f22-ab00-4f9d-99f2-e224ffcd7235.jpg')
+  ];
 
   constructor() { }
 
